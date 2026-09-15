8b6335e392f148037968664247597139a5055345
LGM16 <lucasgmigliore@gmail.com>
2026-09-15 00:37:48 -0300

    Primeras clases creadas

---
+++ Winform-App/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winform_App
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        //Faltaria agregar IdMarca? 
+        public Marca Marca { get; set; }
+        //IdCategoria? 
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; } = new List<Imagen>();
+    }
+}
---
+++ Winform-App/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winform_App
+{
+    public class Imagen
+    {
+        public int Id { get; set; }
+        public int ArticuloId { get; set; }
+        public string Url { get; set; }
+    }
+}
