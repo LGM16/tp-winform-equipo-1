c0608759f727fb277e32bf569103ace2e56a4c3b
LGM16 <lucasgmigliore@gmail.com>
2026-09-20 16:25:49 -0300

    update frm

--- Winform-App/frmAltaArticulo.cs
+++ Winform-App/frmAltaArticulo.cs
@@ -86,20 +86,29 @@ namespace Winform_App
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            Articulo art = new Articulo();
+            //Articulo art = new Articulo();
             ArticuloNegocio negocio = new ArticuloNegocio();
             //  validaciones + guardar
             try
             {
-                art.Codigo = txtCodigo.Text;
-                art.Nombre = txtNombre.Text;
-                art.Descripcion = txtDescripcion.Text;
-                art.Marca = (Dominio.Marca)cboMarca.SelectedItem;
-                art.Categoria = (Dominio.Categoria)cboCategoria.SelectedItem;
-                art.Precio = decimal.Parse(txtPrecio.Text);
+                articulo.Codigo = txtCodigo.Text;
+                articulo.Nombre = txtNombre.Text;
+                articulo.Descripcion = txtDescripcion.Text;
+                articulo.Marca = (Dominio.Marca)cboMarca.SelectedItem;
+                articulo.Categoria = (Dominio.Categoria)cboCategoria.SelectedItem;
+                articulo.Precio = decimal.Parse(txtPrecio.Text);
 
-                negocio.agregar(art);
-                MessageBox.Show("Agregado de manera exitosa");
+                if (articulo.Id != 0)
+                {
+                    negocio.modificar(articulo);
+                    MessageBox.Show("Modificado de manera exitosa", "Atención");
+                }
+                else
+                {
+                    negocio.agregar(articulo);
+                    MessageBox.Show("Agregado de manera exitosa", "Atención");
+                }
+
                 Close();
             }
             catch (Exception ex)
